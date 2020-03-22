14e151fc92381d337c18e63cd6c8140723fe7549
Abdusamadzoda Abdulaziz <abdulaziz.abdurasul1999@gmail.com>
2020-03-22 11:09:19 +0800

    server js has been changed

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
-cont PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 
 const server = http.createServer(function(req,res){
     fs.readFile( "index.html", function(err, data) {
