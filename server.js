456b2be286cfbe45d89bb9514931f0bad94aab89
Abdusamadzoda Abdulaziz <abdulaziz.abdurasul1999@gmail.com>
2020-03-22 10:07:44 +0800

    All files has been uplouded as draft now

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+const server = http.createServer(function(req,res){
+    fs.readFile('index.html', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+      });
+
+}).listen(8089);
+
+console.log('Server is lisening on port 8089');
